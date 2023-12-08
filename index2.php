b2b491d1af54bdca0d73290dbbb95c7b4a4814f5
berkay.polat <berkay.polat@eu.rhenus.com>
2023-12-08 09:47:12 +0100

    tabelle als index seite

---
+++ index2.php
@@ -0,0 +1,48 @@
+<?php 
+    include("connection.php");
+    include("login.php")
+    ?>
+    
+<html>
+    <head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@700&family=Fjalla+One&family=Josefin+Sans:wght@700&family=Kanit:wght@700&family=Kdam+Thmor+Pro&family=Roboto:ital,wght@1,900&family=Rubik:wght@500&family=Vina+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+        <div id="form">
+            <h1>Bundesliga-Tippspiel</h1>
+            <form name="form" action="login.php" onsubmit="return isvalid()" method="POST">
+                <label>Username: </label>
+                <input type="text" id="user" name="user"></br></br>
+                <label>Password: </label>
+                <input type="password" id="pass" name="pass"></br></br>
+                <input type="submit" id="btn" value="Login" name = "submit"/>
+            </form>
+        </div>
+        <script>
+            function isvalid(){
+                var user = document.form.user.value;
+                var pass = document.form.pass.value;
+                if(user.length=="" && pass.length==""){
+                    alert(" Username and password field is empty!!!");
+                    return false;
+                }
+                else if(user.length==""){
+                    alert(" Username field is empty!!!");
+                    return false;
+                }
+                else if(pass.length==""){
+                    alert(" Password field is empty!!!");
+                    return false;
+                }
+                
+            }
+        </script>
+    </body>
+</html>
